602e85b2bf73cfc3412fa559cbb589cb476bb040
JFR <JFR@qq.com>
2019-08-02 20:09:29 +0800

    Modified templates to display message from database

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class User(models.Model):
+class Message(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
--- show/views.py
+++ show/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Message
 # Create your views here.
 def post_list(request):
-    return render(request, 'show/post_list.html', {})
+    posts = Message.objects.order_by('published_date')
+    return render(request, 'show/post_list.html',  {'posts': posts})
